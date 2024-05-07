3a028062dbc4197c20a78a1c55a5ec00d73a3221
arahihe <herreraarahi@gmail.com>
2024-05-07 10:04:36 -0600

    first  attempt

--- src/tests/users.test.js
+++ src/tests/users.test.js
@@ -1,9 +1,27 @@
 // You already have the methods imported
-import { default as expect } from "expect";
-import { getUser, getUsers } from "../users";
+// import { default as expect } from "expect";
+// import { getUser, getUsers } from "../users";
 
 // Make a test to verify if the method getUser(id) works correctly
 // Verify any data of the resulting object you want
 
 // READ documentation
+
+const users = require('../data.js');
+const {
+    getUsersById,
+    // getUsersByUsername
+} = require('../users.js');
+
+describe('getUsersById', () => {
+    it('It should retrieve the correct user for a given ID', () => {
+        const user = users[1];
+        expect(getUsersById(2)).toEqual(user);
+    });
+
+   it('It should fail to get the user', () => {
+    const user = users[3];
+    expect(getUsersById(2)).toEqual(user);
+   })
+});
 
--- src/users.js
+++ src/users.js
@@ -1,19 +1,30 @@
 // Imports small array of users
 // Pretend it's an API request
-const users = require("./data").default;
+const users = require("./data.js");
 
 // Fetches all users
-const getUsers = () => {
-  return users;
-};
+// const getUsers = () => {
+  // return users;
+// };
 
 // Filters users by specific ID
-const getUser = (id) => {
-  return users.find((user) => user.id === id);
-};
+// const getUser = (id) => {
+  // return users.find((user) => user.id === id);
+// };
 
 // test
 // console.log(getUser(3));
 
-module.exports = { getUsers, getUser };
+// module.exports = { getUsers, getUser };
 
+function getUsersById(id) {
+  return users.find(user => user.id === id) || null;
+}
+function getUsersByUsername(username) {
+  return users.find(user => user.username === username) || null;
+}
+
+module.exports = {
+  getUsersById,
+  getUsersByUsername
+};
